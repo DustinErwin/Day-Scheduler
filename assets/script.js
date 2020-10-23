ed478299108f86d9068b82944582914eec95dc97
Galahad <derwin499@gmail.com>
2020-10-23 10:13:37 -0400

    changed some variable names for clarity

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,7 @@
-let timeBlk = $(".time-block");
 let hours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 let m = moment();
 
-$("#currentDay").text(m.format("dddd MMM Mo YYYY"));
+$("#currentDay").text(m.format("dddd LL"));
 
 //Populates page with time blocks
 for (let i = 0; i < 9; i++) {
@@ -12,30 +11,19 @@ for (let i = 0; i < 9; i++) {
   let hour = `<p id='p-${i}' class='hour'>${hours[i]}</p>`;
 
   let input;
-  let fI = getInput('input-'+i)
-
-  
+  let fillInput = getInput("input-" + i);
   if (i + 9 < m.format("H")) {
-    input =
-      `<input id='input-${i}' type='text' class='text-center past time-block'/>`;
+    input = `<input id='input-${i}' type='text' class='text-center past time-block'>`;
   } else if (i + 9 === Math.floor(m.format("H"))) {
-    input =
-      `<input id='input-${i}' type='text' class='text-center present time-block'>`;
+    input = `<input id='input-${i}' type='text' class='text-center present time-block'>`;
   } else {
-    input =
-      `<input id='input-${i}' type='text' class='text-center future time-block'>`;
+    input = `<input id='input-${i}' type='text' class='text-center future time-block'>`;
   }
 
   let btn = `<button id='button-${i}' class='saveBtn'>Save</button>`;
 
-  
-
-  
-
   $("#" + i).append(hour, input, btn);
-  $('#input-'+i).val(fI)
-
-
+  $("#input-" + i).val(fillInput);
 }
 
 //Finds sibling input
@@ -43,8 +31,8 @@ function saveTimeBlock() {
   pairInp = $(this).prev();
   noteSave = pairInp[0];
   note = noteSave.value;
-  inpId = noteSave.getAttribute('id')
-  noteStor(inpId, note) 
+  inpId = noteSave.getAttribute("id");
+  noteStor(inpId, note);
 }
 
 //Stores input
@@ -52,10 +40,10 @@ function noteStor(a, b) {
   localStorage.setItem(a, b);
 }
 
-
+//Retrieves input
 function getInput(a) {
-  let index = new String(a)
-  let input = localStorage.getItem(index)
+  let index = new String(a);
+  let input = localStorage.getItem(index);
   return input;
 }
 
